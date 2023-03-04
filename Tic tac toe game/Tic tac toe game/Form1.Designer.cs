5a849aba9a24957066ee1a053d4e1379ff16bbb3
Panahli001 <57041713+MrPnhl@users.noreply.github.com>
2023-03-04 10:15:27 +0400

    Add files via upload

---
+++ Tic tac toe game/Tic tac toe game/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Tic_tac_toe_game
+{
+    partial class frmmain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnrestart = new System.Windows.Forms.Button();
+            this.btnstart = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.c3 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.a1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnplayer1 = new System.Windows.Forms.Button();
+            this.btnplayer2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnplayer1);
+            this.panel1.Controls.Add(this.btnplayer2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(321, 111);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(32, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 37);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnrestart
+            // 
+            this.btnrestart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnrestart.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnrestart.Location = new System.Drawing.Point(18, 261);
+            this.btnrestart.Name = "btnrestart";
+            this.btnrestart.Size = new System.Drawing.Size(104, 37);
+            this.btnrestart.TabIndex = 2;
+            this.btnrestart.Text = "Restart";
+            this.btnrestart.UseVisualStyleBackColor = true;
+            this.btnrestart.Click += new System.EventHandler(this.btnrestart_Click);
+            // 
+            // btnstart
+            // 
+            this.btnstart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnstart.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnstart.Location = new System.Drawing.Point(7, 304);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(116, 64);
+            this.btnstart.TabIndex = 1;
+            this.btnstart.Text = "Start new game";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.c3);
+            this.panel2.Controls.Add(this.b3);
+            this.panel2.Controls.Add(this.a3);
+            this.panel2.Controls.Add(this.c2);
+            this.panel2.Controls.Add(this.b2);
+            this.panel2.Controls.Add(this.a2);
+            this.panel2.Controls.Add(this.c1);
+            this.panel2.Controls.Add(this.b1);
+            this.panel2.Controls.Add(this.a1);
+            this.panel2.Location = new System.Drawing.Point(12, 129);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(321, 275);
+            this.panel2.TabIndex = 0;
+            // 
+            // c3
+            // 
+            this.c3.BackColor = System.Drawing.Color.SkyBlue;
+            this.c3.Font = new System.Drawing.Font("Dubai", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c3.ForeColor = System.Drawing.Color.Red;
+            this.c3.Location = new System.Drawing.Point(196, 176);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(79, 74);
+            this.c3.TabIndex = 0;
+            this.c3.UseVisualStyleBackColor = false;
+            this.c3.Click += new System.EventHandler(this.c3_Click);
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.Color.SkyBlue;
+            this.b3.Font = new System.Drawing.Font("Dubai", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.ForeColor = System.Drawing.Color.Red;
+            this.b3.Location = new System.Drawing.Point(196, 101);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(79, 74);
+            this.b3.TabIndex = 0;
+            this.b3.UseVisualStyleBackColor = false;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // a3
+            // 
+            this.a3.BackColor = System.Drawing.Color.SkyBlue;
+            this.a3.Font = new System.Drawing.Font("Dubai", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a3.ForeColor = System.Drawing.Color.Red;
+            this.a3.Location = new System.Drawing.Point(196, 26);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(79, 74);
+            this.a3.TabIndex = 0;
+            this.a3.UseVisualStyleBackColor = false;
+            this.a3.Click += new System.EventHandler(this.a3_Click);
+            // 
+            // c2
+            // 
+            this.c2.BackColor = System.Drawing.Color.SkyBlue;
+            this.c2.Font = new System.Drawing.Font("Dubai", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c2.ForeColor = System.Drawing.Color.Red;
+            this.c2.Location = new System.Drawing.Point(118, 176);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(79, 74);
+            this.c2.TabIndex = 0;
+            this.c2.UseVisualStyleBackColor = false;
+            this.c2.Click += new System.EventHandler(this.c2_Click);
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.Color.SkyBlue;
+            this.b2.Font = new System.Drawing.Font("Dubai", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.ForeColor = System.Drawing.Color.Red;
+            this.b2.Location = new System.Drawing.Point(118, 101);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(79, 74);
+            this.b2.TabIndex = 0;
+            this.b2.UseVisualStyleBackColor = false;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // a2
+            // 
+            this.a2.BackColor = System.Drawing.Color.SkyBlue;
+            this.a2.Font = new System.Drawing.Font("Dubai", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a2.ForeColor = System.Drawing.Color.Red;
+            this.a2.Location = new System.Drawing.Point(118, 26);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(79, 74);
+            this.a2.TabIndex = 0;
+            this.a2.UseVisualStyleBackColor = false;
+            this.a2.Click += new System.EventHandler(this.a2_Click);
+            // 
+            // c1
+            // 
+            this.c1.BackColor = System.Drawing.Color.SkyBlue;
+            this.c1.Font = new System.Drawing.Font("Dubai", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c1.ForeColor = System.Drawing.Color.Red;
+            this.c1.Location = new System.Drawing.Point(40, 176);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(79, 74);
+            this.c1.TabIndex = 0;
+            this.c1.UseVisualStyleBackColor = false;
+            this.c1.Click += new System.EventHandler(this.c1_Click);
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.SkyBlue;
+            this.b1.Font = new System.Drawing.Font("Dubai", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.ForeColor = System.Drawing.Color.Red;
+            this.b1.Location = new System.Drawing.Point(40, 101);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(79, 74);
+            this.b1.TabIndex = 0;
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // a1
+            // 
+            this.a1.BackColor = System.Drawing.Color.SkyBlue;
+            this.a1.Font = new System.Drawing.Font("Dubai", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a1.ForeColor = System.Drawing.Color.Red;
+            this.a1.Location = new System.Drawing.Point(40, 26);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(79, 74);
+            this.a1.TabIndex = 0;
+            this.a1.UseVisualStyleBackColor = false;
+            this.a1.Click += new System.EventHandler(this.a1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.btnstart);
+            this.panel3.Controls.Add(this.btnrestart);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Location = new System.Drawing.Point(339, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(135, 392);
+            this.panel3.TabIndex = 1;
+            // 
+            // btnplayer1
+            // 
+            this.btnplayer1.Enabled = false;
+            this.btnplayer1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnplayer1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnplayer1.Location = new System.Drawing.Point(32, 15);
+            this.btnplayer1.Name = "btnplayer1";
+            this.btnplayer1.Size = new System.Drawing.Size(88, 84);
+            this.btnplayer1.TabIndex = 0;
+            this.btnplayer1.Text = "Player 1";
+            this.btnplayer1.UseVisualStyleBackColor = true;
+            // 
+            // btnplayer2
+            // 
+            this.btnplayer2.Enabled = false;
+            this.btnplayer2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnplayer2.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnplayer2.Location = new System.Drawing.Point(197, 15);
+            this.btnplayer2.Name = "btnplayer2";
+            this.btnplayer2.Size = new System.Drawing.Size(88, 84);
+            this.btnplayer2.TabIndex = 0;
+            this.btnplayer2.Text = "Player 2";
+            this.btnplayer2.UseVisualStyleBackColor = true;
+            // 
+            // frmmain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(478, 416);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "frmmain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.frmmain_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnrestart;
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.Button btnplayer2;
+        private System.Windows.Forms.Button btnplayer1;
+    }
+}
+
---
+++ Tic tac toe game/Tic tac toe game/Form1.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_tac_toe_game
+{
+    public partial class frmmain : Form
+    {
+        public frmmain()
+        {
+            InitializeComponent();
+        }
+
+        bool gedis = true;
+        int saygac = 0;
+
+        private void frmmain_Load(object sender, EventArgs e)
+        {
+            panel2.Enabled = false;
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            panel2.Enabled = true;
+            btnplayer1.BackColor = Color.Green;
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void a1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button a = (Button)sender;
+                if (gedis == true)
+                {
+                    a.Text = "X";
+                    
+                    btnplayer2.BackColor = Color.Green;
+                    btnplayer1.BackColor = Color.FloralWhite;
+                }
+                else
+                {
+                    a.Text = "0";
+                    btnplayer1.BackColor = Color.Green;
+                    btnplayer2.BackColor = Color.FloralWhite;
+                }
+                    
+                gedis = !gedis;
+                saygac++;
+                btnstart.Enabled = false;
+                a.Enabled = false;
+                check();
+                
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        public void check()
+        {
+            bool qalibmovcudur = false;
+
+            if ((a1.Text == a2.Text) && (a2.Text == a3.Text) && a1.Text != "")
+                qalibmovcudur = true;
+            else if ((b1.Text == b2.Text) && (b2.Text == b3.Text) && b1.Text != "")
+                qalibmovcudur = true;
+            else if ((c1.Text == c2.Text) && (c2.Text == c3.Text) && c1.Text != "")
+                qalibmovcudur = true;
+
+            else if ((a1.Text == b1.Text) && (b1.Text == c1.Text) && a1.Text != "")
+                qalibmovcudur = true;
+            else if ((a2.Text == b2.Text) && (b2.Text == c2.Text) && a2.Text != "")
+                qalibmovcudur = true;
+            else if ((a3.Text == b3.Text) && (b3.Text == c3.Text) && a3.Text != "")
+                qalibmovcudur = true;
+
+            else if ((a1.Text == b2.Text) && (b2.Text == c3.Text) && a1.Text != "")
+                qalibmovcudur = true;
+            else if ((a3.Text == b2.Text) && (b2.Text == c1.Text) && b2.Text != "")
+                qalibmovcudur = true;
+
+
+            if (qalibmovcudur)
+            {
+                btnrestart.Enabled = true;
+                btnplayer1.BackColor = Color.FloralWhite;
+                btnplayer2.BackColor = Color.FloralWhite;
+                string netice = "";
+                if (gedis)
+                {
+                    netice = "0";
+                }
+                else
+                    netice = "X";
+                MessageBox.Show("Oyunun qalib " + netice + " dir! Davam etmək üçün OK klik edin", "Nəticə");
+            }
+            else if (saygac==9)
+            {
+                btnstart.Enabled = true;
+                MessageBox.Show("Oyun heç-heçə bitdi!  Davam etmək üçün OK klik edin");
+            }
+        }
+
+        private void a2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button a = (Button)sender;
+                if (gedis == true)
+                {
+                    a.Text = "X";
+                    btnplayer2.BackColor = Color.Green;
+                    btnplayer1.BackColor = Color.FloralWhite;
+                }
+                else
+                {
+                    a.Text = "0";
+                    btnplayer1.BackColor = Color.Green;
+                    btnplayer2.BackColor = Color.FloralWhite;
+                }
+                    
+                gedis = !gedis;
+                saygac++;
+                btnstart.Enabled = false;
+                a.Enabled = false;
+                check();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void a3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button a = (Button)sender;
+                if (gedis == true)
+                {
+                    a.Text = "X";
+                    btnplayer2.BackColor = Color.Green;
+                    btnplayer1.BackColor = Color.FloralWhite;
+                }
+                else
+                {
+                    a.Text = "0";
+                    btnplayer1.BackColor = Color.Green;
+                    btnplayer2.BackColor = Color.FloralWhite;
+                }
+                    
+                gedis = !gedis;
+                saygac++;
+                btnstart.Enabled = false;
+                a.Enabled = false;
+                check();
+                
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button a = (Button)sender;
+                if (gedis == true)
+                {
+                    a.Text = "X";
+                    btnplayer2.BackColor = Color.Green;
+                    btnplayer1.BackColor = Color.FloralWhite;
+                }
+                else
+                {
+                    a.Text = "0";
+                    btnplayer1.BackColor = Color.Green;
+                    btnplayer2.BackColor = Color.FloralWhite;
+                }
+                    
+                gedis = !gedis;
+                saygac++;
+                btnstart.Enabled = false;
+                a.Enabled = false;
+                check();
+
+                btnstart.Enabled = false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button a = (Button)sender;
+                if (gedis == true)
+                {
+                    a.Text = "X";
+                    btnplayer2.BackColor = Color.Green;
+                    btnplayer1.BackColor = Color.FloralWhite;
+                }
+                else
+                {
+                    a.Text = "0";
+                    btnplayer1.BackColor = Color.Green;
+                    btnplayer2.BackColor = Color.FloralWhite;
+                }
+                    
+                gedis = !gedis;
+                saygac++;
+                btnstart.Enabled = false;
+                a.Enabled = false;
+                check();
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button a = (Button)sender;
+                if (gedis == true)
+                {
+                    a.Text = "X";
+                    btnplayer2.BackColor = Color.Green;
+                    btnplayer1.BackColor = Color.FloralWhite;
+                }
+                else
+                {
+                    a.Text = "0";
+                    btnplayer1.BackColor = Color.Green;
+                    btnplayer2.BackColor = Color.FloralWhite;
+                }
+                    
+                gedis = !gedis;
+                saygac++;
+                btnstart.Enabled = false;
+                a.Enabled = false;
+                check();
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void c1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button a = (Button)sender;
+                if (gedis == true)
+                {
+                    a.Text = "X";
+                    btnplayer2.BackColor = Color.Green;
+                    btnplayer1.BackColor = Color.FloralWhite;
+                }
+                else
+                {
+                    a.Text = "0";
+                    btnplayer1.BackColor = Color.Green;
+                    btnplayer2.BackColor = Color.FloralWhite;
+                }
+                    
+                gedis = !gedis;
+                saygac++;
+                btnstart.Enabled = false;
+                a.Enabled = false;
+                check();
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void c2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button a = (Button)sender;
+                if (gedis == true)
+                {
+                    a.Text = "X";
+                    btnplayer2.BackColor = Color.Green;
+                    btnplayer1.BackColor = Color.FloralWhite;
+                }
+                else
+                {
+                    a.Text = "0";
+                    btnplayer1.BackColor = Color.Green;
+                    btnplayer2.BackColor = Color.FloralWhite;
+                }
+                    
+                gedis = !gedis;
+                saygac++;
+                btnstart.Enabled = false;
+                a.Enabled = false;
+                check();
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void c3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button a = (Button)sender;
+                if (gedis == true)
+                {
+                    a.Text = "X";
+                    btnplayer2.BackColor = Color.Green;
+                    btnplayer1.BackColor = Color.FloralWhite;
+                }
+                else
+                {
+                    a.Text = "0";
+                    btnplayer1.BackColor = Color.Green;
+                    btnplayer2.BackColor = Color.FloralWhite;
+                }
+                    
+                gedis = !gedis;
+                saygac++;
+                btnstart.Enabled = false;
+                a.Enabled = false;
+                check();
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void btnrestart_Click(object sender, EventArgs e)
+        {
+
+            saygac = 0;
+            gedis = true;
+            a1.Text = "";
+            a1.Enabled = true;
+            a2.Text = "";
+            a2.Enabled = true;
+            a3.Text = "";
+            a3.Enabled = true;
+            b1.Text = "";
+            b1.Enabled = true;
+            b2.Text = "";
+            b2.Enabled = true;
+            b3.Text = "";
+            b3.Enabled = true;
+            c1.Text = "";
+            c1.Enabled = true;
+            c2.Text = "";
+            c2.Enabled = true;
+            c3.Text = "";
+            c3.Enabled = true;
+            btnstart.Enabled = true;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
